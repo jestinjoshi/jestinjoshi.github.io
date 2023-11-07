3e6395baeac5e496768315904890be42f6088105
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2023-11-07 13:01:02 -0500

    next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    const repo = 'jestinjoshi.github.io';
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+const nextConfig = {
+    assetPrefix: `/${repo}/`,
+    basePath: `/${repo}`
+};
 
 module.exports = nextConfig
