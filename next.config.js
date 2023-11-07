59f9a18cdc6979063b445f93edcfea00ed3e6a3e
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2023-11-07 13:21:38 -0500

    Revert "Deploying to main from @ jestinjoshi/jestinjoshi.github.io@783a901afae99e78ad87a4794c916d6a628f09fa 🚀"
    This reverts commit bb0a5d3b593cbfb0e048cc632b0a4fe96e353839.

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    const repo = 'jestinjoshi.github.io';
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: assetPrefix,
+    basePath: basePath
+};
+
+module.exports = nextConfig
