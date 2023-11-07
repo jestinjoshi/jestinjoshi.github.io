7ce389a7ae6a5f4e19b92138df4fe24b0aca533d
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2023-11-07 13:46:30 -0500

    updated workflow

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = ''
 
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    // assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
-}
+// if (isGithubActions) {
+//     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+//     // assetPrefix = `/${repo}/`
+//     basePath = `/${repo}`
+// }
 
 const nextConfig = {
     output: 'export',
