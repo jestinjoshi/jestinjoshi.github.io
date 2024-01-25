be86af1a635b406e16527ab3d8d53d252924d4a4
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2024-01-25 00:12:18 -0500

    fix: linting issue

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,16 +4,16 @@ import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
-    const handleSubmit = (e: SubmitEvent) => {
+    // const handleSubmit = (e: SubmitEvent) => {
 
-    };
+    // };
 
     return (
         <section id="contact" className="py-10">
             <div className="container px-4 mx-auto">
                 <h2 className="text-3xl section-heading mb-10">Contact</h2>
                 <div className="contact-wrap">
-                    <form onSubmit={handleSubmit} action="dialog">
+                    <form action="dialog">
                         <input type="text" name="name" id="name" placeholder="Name" />
                         <input type="email" name="email" id="email" placeholder="Email" />
                         <textarea name="message" id="message" placeholder="Message" />
