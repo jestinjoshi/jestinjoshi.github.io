fa1bfea4c08c33465ce39489254ba4bda31f0de9
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2024-02-22 11:19:30 -0500

    refactor: resume link updated

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -55,7 +55,7 @@ export default function Intro() {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect x="2" y="9" width="4" height="12" /><circle cx="4" cy="4" r="2" /></svg>
                                 <span className="glass absolute w-full h-full rounded-full top-0 left-0 z-[-1] group-hover:scale-100 scale-0 transition-all"></span>
                             </a>
-                            <a className="px-2 text-sm md:text-base md:px-4 py-3 rounded-lg ml-0 transition text-white flex items-center font-semibold resume glass glass-hover group overflow-hidden hover:pl-12 transition-all" href="/Resume_Jestin.pdf" target="_blank" rel="noreferrer">
+                            <a className="px-2 text-sm md:text-base md:px-4 py-3 rounded-lg ml-0 transition text-white flex items-center font-semibold resume glass glass-hover group overflow-hidden hover:pl-12 transition-all" href="/resume.pdf" target="_blank" rel="noreferrer">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-download absolute group-hover:start-4 -start-full transition-all"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
                                 Resume
                             </a>
