6b3cc42acde60af36c42f70a30c6d58596745e4d
Jestin Joshi <34907800+jestinjoshi@users.noreply.github.com>
2024-02-23 17:08:56 -0500

    added hamburger menu

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -42,8 +42,8 @@ export default function Contact() {
                             <motion.div initial={initialFadeUp} animate={fadeIn()}>{emailResponse}</motion.div>
                             :
                             <motion.form exit={{ height: 0 }} initial={{ ...initialFadeUp, height: 'auto' }} animate={fadeIn()} onSubmit={handleSubmit} method="dialog" className='flex gap-5 lg:gap-10 flex-wrap'>
-                                <motion.input initial={initialFadeUp} whileInView={fadeIn(0.7)} viewport={{ once: true, root: scrollRef }} required type="text" name="name" id="name" placeholder="Name" aria-label="Name" className='flex-1 p-4 rounded-md glass' />
-                                <motion.input initial={initialFadeUp} whileInView={fadeIn(0.9)} viewport={{ once: true, root: scrollRef }} required type="email" name="email" id="email" placeholder="Email" aria-label="Email" className='flex-1 p-4 rounded-md glass' />
+                                <motion.input autoComplete='off' initial={initialFadeUp} whileInView={fadeIn(0.7)} viewport={{ once: true, root: scrollRef }} required type="text" name="name" id="name" placeholder="Name" aria-label="Name" className='flex-1 p-4 rounded-md glass' />
+                                <motion.input autoComplete='off' initial={initialFadeUp} whileInView={fadeIn(0.9)} viewport={{ once: true, root: scrollRef }} required type="email" name="email" id="email" placeholder="Email" aria-label="Email" className='flex-1 p-4 rounded-md glass' />
                                 <motion.textarea initial={initialFadeUp} whileInView={fadeIn(1.1)} viewport={{ once: true, root: scrollRef }} required rows={4} name="message" id="message" placeholder="Message" aria-label="Message" className='w-full p-4 rounded-md glass resize-none' />
                                 <motion.input initial={initialFadeUp} whileInView={fadeIn(1.3)} viewport={{ once: true, root: scrollRef }} type="submit" value="Send" className='send py-3 px-10 text-white rounded-md cursor-pointer glass glass-hover' />
                             </motion.form>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,16 @@
 import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 import { fadeIn, initialFadeDown } from "../animations";
 
-interface HeaderProps {
+type HeaderProps = {
     mainRef: React.RefObject<HTMLDivElement>;
 }
 
 let initialHighlightOffset = 40;
 
-const Header: React.FC<HeaderProps> = ({ mainRef }) => {
-    const menu = ["about", "skills", "experience", "education", "portfolio", "contact"];
+const menu = ["about", "skills", "experience", "education", "portfolio", "contact"];
+
+const Header = ({ mainRef }: HeaderProps) => {
 
     const headerRef = useRef<HTMLElement>(null);
     const [targetStyle, setTargetStyle] = useState({});
@@ -92,15 +93,109 @@ const Header: React.FC<HeaderProps> = ({ mainRef }) => {
                     </nav>
 
                     <div style={targetStyle} className="highlight absolute h-1 bg-white transition-all rounded-full"></div>
-
-                    <div className="hamburger hidden">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
+                    <HamburgerMenu handleHashClick={handleHashClick} />
                 </div>
             </div>
         </motion.header>
+    )
+}
+
+type HamburgerProps = {
+    handleHashClick: React.MouseEventHandler
+}
+
+const sidebarVariant = {
+    open: (height = 1000) => ({
+        clipPath: `circle(${height * 2 + 200}px at right top)`,
+        transition: {
+            type: "spring",
+            stiffness: 20,
+            restDelta: 2
+        }
+    }),
+    closed: {
+        clipPath: "circle(0px at right top)",
+        transition: {
+            delay: 0.5,
+            type: "spring",
+            stiffness: 400,
+            damping: 40
+        }
+    }
+};
+
+const menuContainerVariant = {
+    open: {
+        transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+    },
+    closed: {
+        transition: { staggerChildren: 0.05, staggerDirection: -1 }
+    }
+};
+
+const menuVariant = {
+    open: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            y: { stiffness: 1000, velocity: -100 }
+        }
+    },
+    closed: {
+        y: 50,
+        opacity: 0,
+        transition: {
+            y: { stiffness: 1000 }
+        }
+    }
+};
+
+const HamburgerMenu = (props: HamburgerProps) => {
+    const [isOpen, toggleOpen] = useCycle(false, true);
+    const containerRef = useRef(null);
+    const containerHeightRef = useRef(1000);
+
+    const setContainerHeight = () => {
+        if (containerRef.current) {
+            containerHeightRef.current = (containerRef.current as Element).clientHeight;
+        }
+    };
+
+    const handleHamburgerMenuClick: React.MouseEventHandler<HTMLAnchorElement> = e => {
+        props.handleHashClick(e);
+        toggleOpen();
+    }
+
+    useEffect(() => {
+        setContainerHeight();
+        window.addEventListener('resize', setContainerHeight)
+    }, [containerRef])
+
+    return (
+        <>
+            <motion.nav
+                ref={containerRef}
+                initial="closed"
+                animate={isOpen ? "open" : "closed"}
+                variants={sidebarVariant}
+                custom={containerHeightRef.current}
+                className="absolute right-0 top-0 z-[1] w-full h-screen text-center flex items-center justify-center bg-slate-900"
+            >
+                <motion.ul variants={menuContainerVariant} className="flex flex-col gap-6">
+                    {menu.map((m, i) =>
+                        <motion.li key={m} className="capitalize" whileTap={{ scale: 0.95 }} variants={menuVariant}>
+                            <a className="hover:opacity-75 text-xl" onClick={handleHamburgerMenuClick} href={`#${m}`}>{m}</a>
+                        </motion.li>
+                    )}
+                </motion.ul>
+            </motion.nav>
+            <div className={`hamburger z-[1] sm:hidden ${isOpen ? 'open' : ''}`} onClick={() => toggleOpen()}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </>
     )
 }
 
